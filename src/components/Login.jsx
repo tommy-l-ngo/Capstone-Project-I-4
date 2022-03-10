4ba2e1b401ec44cb937b125160c7306ed3fae8f2
zanthony42 <zanthony456@gmail.com>
2022-03-09 20:43:03 -0600

    set up login page

---
+++ src/components/Login.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Login extends Component {
+    
+    render() { 
+        return (
+        <>
+        <h1>Log In</h1>
+        </>
+        );
+    }
+}
+ 
+export default Login;
