0dba667a37c2f574dcac708a5a63bd0cfb0ef0c4
landerick0s <erick_2358@yahoo.com>
2022-03-12 20:09:30 -0600

    Add Register directory

---
+++ src/components/Register/Register.js
@@ -0,0 +1,18 @@
+import {React, Component} from "react";
+import { Form, Button, Card, Container } from "react-bootstrap";
+import "../../firebase";
+import {getDatabase, set, ref } from "firebase/database";
+import { Link } from 'react-router-dom';
+
+export default class Register extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            userFullName: "",
+            userEuid: "",
+            userDOB: "",
+            userPassword: "",
+        };
+    }    
+}
