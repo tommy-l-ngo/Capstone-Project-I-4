b17d37e662c94bcb2400148ca501a306d5148add
anthony <zanthony456@gmail.com>
2022-04-01 17:16:33 -0500

    meetings added via date click, connected to database

--- src/components/Calendar/Calendar.js
+++ src/components/Calendar/Calendar.js
@@ -1,15 +1,65 @@
 import Navbar from "../Dashboard/Navbar"
-import React from 'react'
+import React , {useState} from 'react'
 import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
+import interactionPlugin from "@fullcalendar/interaction" // needed for dayClick
+import { getDatabase, set, ref, get, child, dbRef } from "firebase/database";
+
 export function Calendar() {
+    const db = getDatabase();
+    const userEUID = "exa0012"; //FIX ME: replace with logged in user
+    const [userEvents, setUserEvents] = useState("");
+   
+    function handleDateclick(selection){ // "selection" object passed when a date is clicked on calendar
+        // FIX ME: Custom pop-up box should be used instead of generic window prompt
+        // meeting details
+        const meetHost = userEUID;
+        const meetGuests = "exa0011, exa0013";
+        const meetDate = selection.dateStr;
+        const meetTime = "12:00";
+        const meetProj = "Project 1"
+        const meetTitle = window.prompt("Enter a title:","Meeting 1");
+        const meetNotes = "None";
+        const meetID = meetDate+"_"+meetTime+"_"+meetTitle;// prevents duplicate meetings
+        
+        // add meeting to database
+        set(ref(db, "calendars/" + userEUID + "/" + meetID), {
+            host: meetHost,
+            guests: meetGuests,
+            date: meetDate,
+            time: meetTime,
+            project: meetProj,
+            title: meetTitle,
+            notes: meetNotes
+        });
+        console.log("Meeting added");
+    }
+
+    function handleDisplayMeetings(){
+        console.log("Meetings:");
+
+        // get meetings of userEUID from database
+        get(ref(db, "calendars/" + userEUID)).then((snapshot) => {
+        if (snapshot.exists()) {
+            console.log(snapshot.val(),snapshot.size);
+        } else {
+            console.log("No data available");
+        }
+        }).catch((error) => {
+        console.error(error);
+        });
+    }
+    
+
     return (<div style={{backgroundColor: 'white' }}>
         <>
             <Navbar />
+            <button onClick={handleDisplayMeetings}>TEST BUTTON</button>
             <FullCalendar
-                
-                plugins={[dayGridPlugin]}
+                plugins={[dayGridPlugin, interactionPlugin]}
                 initialView="dayGridMonth"
+                dateClick={handleDateclick}
+                events = {userEvents}
             />
         </>
 
