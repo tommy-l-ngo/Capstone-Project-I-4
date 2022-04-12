f304995da622900a45e83b135d6e2137671b7176
vpranav99 <37090166+vpranav99@users.noreply.github.com>
2022-04-12 16:00:27 -0500

    Fully implemented dropdown

--- src/components/Create-project/MultiSelect.js
+++ src/components/Create-project/MultiSelect.js
@@ -1,15 +1,67 @@
 import React from 'react'
 import Select from 'react-select'
+//import "./firebase.js"
+import "./confg.js"
+import { child,Database,DataSnapshot,get, getDatabase, push, query, update } from "firebase/database";
+import firebase from "firebase/compat/app"
+import { ref, set} from "firebase/database"
+
+
 
 const options=[
-    {label:'John Doe jtd1234', value:'john doe'},
-    {label:'Mary Sue mjs5123', value:'mary sue'},
-    {label:'Sam McDonald snd2331', value:'sam mcdonald'},
+    
+   {label:'', value:''},
 ]
+function getUserData() 
+{ 
+    var student_ids = new Array();
+    var count = 0; 
+    const db = ref(getDatabase()); 
+    get(child(db,"users/"))
+    .then((snapshot)=>{
+        snapshot.forEach(function(childSnapshot){
+        if(childSnapshot.child("role").val() == "student")
+        {
+          
+        
+           var id = childSnapshot.child("eUID").val(); 
+
+           options.push({label:id,value:id});
+           
+          
+           
+            
+          
+           
+         
+           
+
+          
+            
+        }
+       
+
+        })
+        
+    })
+
+    
+    
+   
+    
+    
+    
+    
+    
+}
+
+
 // put student names from database here ^
 
 function MultiSelect(props){
- //console.log("this is handle props", handleOnChange)
+
+    getUserData()
+   
     return <div>
         <Select
         isMulti 
