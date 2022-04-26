8419a2491dcf64c1e652b073bad159d2f5f37175
vpranav99 <37090166+vpranav99@users.noreply.github.com>
2022-04-26 16:40:39 -0500

    configuration JSON

---
+++ src/components/Create-project/confg.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAu1kdEKPqTfL1XIjDF2l8rfG53FcdtVSM",
+    authDomain: "capstone-i4.firebaseapp.com",
+    databaseURL: "https://capstone-i4-default-rtdb.firebaseio.com",
+    projectId: "capstone-i4",
+    storageBucket: "capstone-i4.appspot.com",
+    messagingSenderId: "768427043765",
+    appId: "1:768427043765:web:6643185734fe346ddd07fc",
+    measurementId: "G-X8E63KZMT3"
+};
