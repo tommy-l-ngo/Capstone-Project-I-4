842404e0c23073d908209f6500e9aae41c72d9ae
anthony <zanthony456@gmail.com>
2022-10-19 12:06:11 -0500

    added user data from db to admin page

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -13,6 +13,9 @@ export function AdminPage() {
     const user = getAuth().currentUser;
     const db = getDatabase();
 
+    const [userData, setUserData] = useState([]);
+    let userDataList = [];
+
     // connect to auth
     if (user !== null) {
         // The user object has basic properties such as display name, email, etc.
@@ -23,23 +26,36 @@ export function AdminPage() {
         console.log("No User");
     }
 
-    // connect to db
-    get(ref(db, "users/")).then((snapshot) => {
-        if (snapshot.exists()) {
-            //loop through users in db
-            snapshot.forEach((eventShot) => {
-                console.log(eventShot.val().email);
-            })
-
-        } else {
-            console.log("No user data available");
-        }
-    }).catch((error) => {
-        console.error(error);
-    });
-
-
-
+    // get user data from db
+    function getUserData()
+    {
+        get(ref(db, "users/")).then((snapshot) => {
+            if (snapshot.exists()) {
+                //loop through users in db
+                snapshot.forEach((eventShot) => {
+                    //console.log(eventShot.val().email);
+                    var userEmail = eventShot.val().email;
+                    var userEUID = eventShot.val().eUID;
+                    var userFirst = eventShot.val().firstName;
+                    var userLast = eventShot.val().lastName;
+                    var userRole = eventShot.val().role;
+                    var userVals = {email: userEmail, euid: userEUID, role: userRole};
+                    userDataList.push(userVals);
+                })
+                setUserData(userDataList);
+            } else {
+                console.log("No user data available");
+            }
+        }).catch((error) => {
+            console.error(error);
+        });
+    }// getUserData()
+    
+    useEffect(() => {
+        getUserData();
+    }, []);
+    
+    
     // rendered content
     return (
         <div>
@@ -60,15 +76,19 @@ export function AdminPage() {
                             <div className="table-container">
                                 {/* <div className="table-content"> */}
                                 <table className="users-table">
+                                
+                                {userData.map((val, key) => { /* map userData array into table*/
+                                    return (
+                                        <tr key={key}>
+                                            <td>{val.email}</td>
+                                            <td>{val.euid}</td>
+                                            <td>{val.role}</td>
+                                            <td className="delete-user"><button className="delete-user-button">X</button></td>
+                                        </tr>
+                                    )
+                                    })}
+
                                 <tr>
-                                        <th className="title">Email</th>
-                                        <th className="title">First Name</th>
-                                        <th className="title">Last Name</th>
-                                        <th className="title">Role</th>
-                                        <th className="title">eUID</th>
-                                        <th className="delete-user-header"></th>
-                                    </tr>
-                                <tr>
                                         <td className="personal-info">someemail@unt.edu</td>
                                         <td className="personal-info">Eagle</td>
                                         <td className="personal-info">Denton</td>
@@ -76,75 +96,7 @@ export function AdminPage() {
                                         <td className="personal-info">abc0020</td>
                                         <td className="delete-user"><button className="delete-user-button">X</button></td>
                                     </tr>
-                                    <tr>
-                                        <td className="personal-info">anotherlonger23email@unt.edu</td>
-                                        <td className="personal-info">Discovery</td>
-                                        <td className="personal-info">Park</td>
-                                        <td className="personal-info">Student</td>
-                                        <td className="personal-info">abc0234</td>
-                                        <td className="delete-user"><button className="delete-user-button">X</button></td>
-                                    </tr><tr>
-                                        <td className="personal-info">someemail@unt.edu</td>
-                                        <td className="personal-info">Eagle</td>
-                                        <td className="personal-info">Denton</td>
-                                        <td className="personal-info">Student</td>
-                                        <td className="personal-info">abc0020</td>
-                                        <td className="delete-user"><button className="delete-user-button">X</button></td>
-                                    </tr>
-                                    <tr>
-                                        <td className="personal-info">anotherlonger23email@unt.edu</td>
-                                        <td className="personal-info">Discovery</td>
-                                        <td className="personal-info">Park</td>
-                                        <td className="personal-info">Student</td>
-                                        <td className="personal-info">abc0234</td>
-                                        <td className="delete-user"><button className="delete-user-button">X</button></td>
-                                    </tr><tr>
-                                        <td className="personal-info">someemail@unt.edu</td>
-                                        <td className="personal-info">Eagle</td>
-                                        <td className="personal-info">Denton</td>
-                                        <td className="personal-info">Student</td>
-                                        <td className="personal-info">abc0020</td>
-                                        <td className="delete-user"><button className="delete-user-button">X</button></td>
-                                    </tr>
-                                    <tr>
-                                        <td className="personal-info">anotherlonger23email@unt.edu</td>
-                                        <td className="personal-info">Discovery</td>
-                                        <td className="personal-info">Park</td>
-                                        <td className="personal-info">Student</td>
-                                        <td className="personal-info">abc0234</td>
-                                        <td className="delete-user"><button className="delete-user-button">X</button></td>
-                                    </tr><tr>
-                                        <td className="personal-info">someemail@unt.edu</td>
-                                        <td className="personal-info">Eagle</td>
-                                        <td className="personal-info">Denton</td>
-                                        <td className="personal-info">Student</td>
-                                        <td className="personal-info">abc0020</td>
-                                        <td className="delete-user"><button className="delete-user-button">X</button></td>
-                                    </tr>
-                                    <tr>
-                                        <td className="personal-info">anotherlonger23email@unt.edu</td>
-                                        <td className="personal-info">Discovery</td>
-                                        <td className="personal-info">Park</td>
-                                        <td className="personal-info">Student</td>
-                                        <td className="personal-info">abc0234</td>
-                                        <td className="delete-user"><button className="delete-user-button">X</button></td>
-                                    </tr>
-                                    <tr>
-                                        <td className="personal-info">someemail@unt.edu</td>
-                                        <td className="personal-info">Eagle</td>
-                                        <td className="personal-info">Denton</td>
-                                        <td className="personal-info">Student</td>
-                                        <td className="personal-info">abc0020</td>
-                                        <td className="delete-user"><button className="delete-user-button">X</button></td>
-                                    </tr>
-                                    <tr>
-                                        <td className="personal-info">anotherlonger23email@unt.edu</td>
-                                        <td className="personal-info">Discovery</td>
-                                        <td className="personal-info">Park</td>
-                                        <td className="personal-info">Student</td>
-                                        <td className="personal-info">abc0234</td>
-                                        <td className="delete-user"><button className="delete-user-button">X</button></td>
-                                    </tr>
+                                    
                                 </table>
 
                                 {/* </div> */}
