c8ca07e7bdc907d5064e2b07fe070d58d2865810
tommy-l-ngo <tommyngo2@my.unt.edu>
2022-10-20 01:03:40 -0500

    Connected File Upload to Firebase / Small Design Changes

--- src/components/FileUpload/FileUpload.js
+++ src/components/FileUpload/FileUpload.js
@@ -1,37 +1,66 @@
 import React, {useState} from 'react';
-import axios from 'axios';
+import { storage } from "../../firebase";
+import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import Progress from './Progress';
+import './FileUpload.css'
+import '../Dashboard/Button.css'
 
 function FileUpload() {
 
   const [file, setFile] = useState()
+
+  // progress
+  const [percent, setPercent] = useState(0)
 
   function handleChange(event) {
     setFile(event.target.files[0])
   }
   
   function handleSubmit(event) {
-    event.preventDefault()
-    const url = 'http://localhost:3000/uploadFile';
-    const formData = new FormData();
-    formData.append('file', file);
-    formData.append('fileName', file.name);
-    const config = {
-      headers: {
-        'content-type': 'multipart/form-data',
-      },
-    };
-    axios.post(url, formData, config).then((response) => {
-      console.log(response.data);
-    });
+    if (!file) {
+      alert("Please upload an image first!");
+    }
 
+    const storageRef = ref(storage, `/files/${file.name}`);
+ 
+    // progress can be paused and resumed. It also exposes progress updates.
+    // Receives the storage reference and the file to upload.
+    const uploadTask = uploadBytesResumable(storageRef, file);
+
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+          const percent = Math.round(
+              (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+          );
+
+          // Clear percentage
+          setTimeout(() => setPercent(0), 10000);
+
+          // update progress
+          setPercent(percent);
+      },
+      (err) => console.log(err),
+      () => {
+          // download url
+          getDownloadURL(uploadTask.snapshot.ref).then((url) => {
+              console.log(url);
+          });
+        }
+    );
   }
 
   return (
     <div className="File-Upload">
         <form onSubmit={handleSubmit}>
           <h4>Upload a File</h4>
-          <input type="file" onChange={handleChange}/>
-          <button type="submit">Upload</button>
+          <div className='fileSelector'>
+            <input type="file" onChange={handleChange}/>
+            <button className='uploadBtn' type="submit">Upload</button>
+          </div>
+          <div className='progress-bar'>
+            <Progress percentage={percent}/>
+          </div>
         </form>
     </div>
   );
