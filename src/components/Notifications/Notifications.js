567ca4bfa6ff064c0453bf56f7ab1a35de518e24
tommy-l-ngo <tommyngo2@my.unt.edu>
2022-11-10 23:46:24 -0600

    Scrollable Navigation bar

--- src/components/Notifications/Notifications.js
+++ src/components/Notifications/Notifications.js
@@ -103,6 +103,7 @@ const Notifications = (props) => {
                                 notifs.map((item, index)=>{
                                     return (
                                         <div>
+                                            <button className="notifs-btn">
                                             <button className="delete-btn" onClick={event => handleDelete(event, item.key)}>
                                                 <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16">
                                                     <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
@@ -114,7 +115,8 @@ const Notifications = (props) => {
                                                 <ul>
                                                     <li className="notif_date">{item.date}</li>
                                                 </ul>
-                                            </li>
+                                                </li>
+                                                </button>
                                             <hr></hr>
                                         </div>
                                     )
